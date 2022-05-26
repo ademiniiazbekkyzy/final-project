fb06e1ec9bd6c280f99d691b8aad0af5dd8e3fcf
ademiniiazbekkyzy <ademi.niiazbekkyzy@mail.ru>
2022-05-26 22:49:46 +0600

    added main window

--- main.py
+++ main.py
@@ -1,60 +1,21 @@
-import os.path
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import *
 
-from PIL import Image, ImageDraw, ImageFont
-
-path = './images'
-new_path = './updated images//'
-dirs = os.listdir(path)
+import sys
 
 
-def crop():
-    for i in dirs:
-        fullpath = os.path.join(path, i)
-        if os.path.isfile(fullpath):
-            image = Image.open(fullpath)
-            file_name = os.path.basename(fullpath)
-            f, e = os.path.splitext(file_name)
-
-            width, height = image.size
-            new_width, new_height = 1080, 1080
-
-            left = (width - new_width) / 2
-            top = (height - new_height) / 2
-            right = (width + new_width) / 2
-            bottom = (height + new_height) / 2
-
-            new_image = image.crop((left, top, right, bottom))
-            new_image.save(new_path + f + '.jpg', "JPEG", quality=100)
+class MainWindow(QMainWindow):
+    def __init__(self, *args, **kwargs):
+        super(MainWindow, self).__init__(*args, **kwargs)
+        self.setWindowTitle('My Awesome App!')
 
 
-crop()
+app = QApplication(sys.argv)
 
-water_path = './updated images//'
-water_dirs = os.listdir(water_path)
+window = MainWindow()
+window.show()
 
+# run the app
+app.exec_()
 
-def watermark():
-    for i in water_dirs:
-        fullpath = os.path.join(water_path, i)
-        if os.path.isfile(fullpath):
-            image = Image.open(fullpath)
-            file_name = os.path.basename(fullpath)
-            f, e = os.path.splitext(file_name)
-
-            width, height = image.size
-            draw = ImageDraw.Draw(image)
-            text = "AUSTIN TEXAS"
-
-            font = ImageFont.truetype('Arial.ttf', 40)
-            text_width, text_height = draw.textsize(text, font)
-
-            margin = 50
-            x = width - text_width - margin
-            y = height - text_height - margin
-
-            draw.text((x, y), text, font=font)
-            image = image.convert(mode='L')
-            image.save(water_path + f + '.jpg', 'JPEG')
-
-
-watermark()
