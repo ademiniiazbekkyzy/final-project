1ab44dcec76335e1261e38f0f61a99c4ff10cf90
ademiniiazbekkyzy <ademi.niiazbekkyzy@mail.ru>
2022-05-29 20:15:54 +0600

    added resize

--- main.py
+++ main.py
@@ -3,16 +3,9 @@ try:
 except ImportError:
     from tkinter import *
 
-from tkinter import *
-
 import tkinter as tk
 from PIL import ImageTk, Image
-from tkinter import Text
-
-# root = Tk()
-# root.title('Ademi')
-# root.geometry('300x700')
-# root.iconbitmap('c')
+from tkinter import Text, filedialog
 
 
 class MarkApp(tk.Tk):
@@ -36,7 +29,54 @@ class MainWindow(tk.Frame):
         self.uni_logo = Image.open("images/logo1.jpeg")
         self.uni_logo = ImageTk.PhotoImage(image=self.uni_logo)
         tk.Label(self, image=self.uni_logo,).pack()
-        tk.Button(self, text="Copyright", command=lambda: master.switch_frame(CopyRightWindow),).pack()
+        tk.Button(
+            self,
+            text="Image Processing",
+            command=lambda: master.switch_frame(ImageProcessingPage),
+        ).pack()
+        tk.Button(self, text="Copyright", command=lambda: master.switch_frame(CopyRightWindow), ).pack()
+
+
+class ImageProcessingPage(tk.Frame):
+    def __init__(self, master):
+        tk.Frame.__init__(self, master)
+        self.path = None
+        self.canvas = None
+        tk.Label(self, text="Image Processing").pack(
+            side="top", fill="x", pady=20
+        )
+        self.configure()
+        tk.Button(
+            self,
+            text="Return to Home page",
+            command=lambda: master.switch_frame(MainWindow),
+        ).pack()
+        self.image = None
+        self.filter_image = None
+
+    def configure(self, *args, **kwargs):
+        self.canvas = Canvas(self, width=650, height=350)
+        self.canvas.pack()
+
+        tk.Button(self, text="Resize", command=self.resize, ).pack()
+
+    def resize(self):
+        max_size = (590, 350)
+        self.path = filedialog.askopenfilename()
+
+        if self.path:
+            self.image = Image.open(self.path)
+            self.image.thumbnail(max_size)
+            self.image = self.image.resize((100, 100), Image.ANTIALIAS)
+            import warnings
+            warnings.filterwarnings('error')
+
+            try:
+                warnings.warn("deprecated", DeprecationWarning)
+            except Warning as e:
+                print(e)
+            self.image = ImageTk.PhotoImage(self.image)
+            self.canvas.create_image(0, 1, image=self.image, anchor="nw")
 
 
 class CopyRightWindow(tk.Frame):
@@ -61,58 +101,3 @@ class CopyRightWindow(tk.Frame):
 if __name__ == "__main__":
     app = MarkApp()
     app.mainloop()
-
-
-# def forward(image_number):
-#     global my_label
-#     global button_back
-#     global button_forward
-#     my_label.grid_forget()
-#     my_label = Label(image=image_list[image_number - 1])
-#     button_forward = Button(root, text='>>', command=lambda: forward(image_number+1))
-#     button_back = Button(root, text='<<', command=lambda: backward(image_number-1))
-#
-#     if image_number == 2:
-#         button_forward = Button(root, text='>>', state=DISABLED)
-#     my_label.grid(row=0, column=0, columnspan=3)
-#     button_back.grid(row=1, column=0)
-#     button_forward.grid(row=1, column=2)
-#
-#
-# def backward(image_number):
-#     global my_label
-#     global button_back
-#     global button_forward
-#     my_label.grid_forget()
-#     my_label = Label(image=image_list[image_number - 1])
-#     button_forward = Button(root, text='>>', command=lambda: forward(image_number + 1))
-#     button_back = Button(root, text='<<', command=lambda: backward(image_number - 1))
-#
-#     if image_number == 2:
-#         button_back = Button(root, text='<<', state=DISABLED)
-#     my_label.grid(row=0, column=0, columnspan=3)
-#     button_back.grid(row=1, column=0)
-#     button_forward.grid(row=1, column=2)
-
-
-# img1 = ImageTk.PhotoImage(Image.open('images/logo1.jpeg'))
-# img2 = ImageTk.PhotoImage(Image.open('images/logo2.png'))
-# image_list = [img1, img2]
-#
-# my_label = Label(image=img1)
-# button_back = Button(root, text='<<', command=backward, state=DISABLED)
-# button_forward = Button(root, text='>>', command=lambda: forward(1))
-# button_exit = Button(root, text='Exit App', command=root.quit)
-# my_label.grid(row=0, column=0, columnspan=3)
-# button_back.grid(row=1, column=0)
-# # button_exit.grid(row=1, column=1)
-# button_forward.grid(row=1, column=2)
-
-
-# img2 = ImageTk.PhotoImage(file='images/logo2.jpeg')
-#
-# my_label = Label(root, image=img2)
-# my_label.pack(pady=20)
-
-# button_quit = Button(root, text='Exit Program', command=root.quit)
-# button_quit.pack()
